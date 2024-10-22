d361c01ab2e8c3c0152a336f7954228df1b187e6
hayotbek2008 <hmuzfrov@gmail.com>
2024-10-22 15:24:00 +0500

    js

--- js/index.js
+++ js/index.js
@@ -13,19 +13,19 @@
 
 
 
-// let students = [
-//   { name: "john", age: 20 },
-//   { name: "Doe", age: 18 },
-//   { name: "Diaz", age: 25 },
-//   { name: "Brahim", age: 20 },
-//   { name: "Alex", age: 24 },
-//   { name: "David", age: 12 },
+let students = [
+  { name: "john", age: 20 },
+  { name: "Doe", age: 18 },
+  { name: "Diaz", age: 25 },
+  { name: "Brahim", age: 20 },
+  { name: "Alex", age: 24 },
+  { name: "David", age: 12 },
 
-// ]
-// function studentsUser(arg) {
-//   let res = arg.filter(function (value) {
-//     return value.age >= 18
-//   })
-//   return res;
-// }
-// console.log(studentsUser(students));
+]
+function studentsFil(arg) {
+  let res = arg.filter(function (value) {
+    return value.age >= 18
+  })
+  return res;
+}
+console.log(studentsFil(students));
