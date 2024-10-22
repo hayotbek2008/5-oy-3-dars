db72ac7ae09906a75ca86d0db5aff83c98cfff09
hayotbek2008 <hmuzfrov@gmail.com>
2024-10-22 15:48:01 +0500

    this class

--- js/index.js
+++ js/index.js
@@ -13,19 +13,62 @@
 
 
 
-let students = [
-  { name: "john", age: 20 },
-  { name: "Doe", age: 18 },
-  { name: "Diaz", age: 25 },
-  { name: "Brahim", age: 20 },
-  { name: "Alex", age: 24 },
-  { name: "David", age: 12 },
+// let students = [
+//   { name: "john", age: 20 },
+//   { name: "Doe", age: 18 },
+//   { name: "Diaz", age: 25 },
+//   { name: "Brahim", age: 20 },
+//   { name: "Alex", age: 24 },
+//   { name: "David", age: 12 },
 
-]
-function studentsFil(arg) {
-  let res = arg.filter(function (value) {
-    return value.age >= 18
-  })
-  return res;
+// ]
+// function studentsFil(arg) {
+//   let res = arg.filter(function (value) {
+//     return value.age >= 18
+//   })
+//   return res;
+// }
+// console.log(studentsFil(students));
+
+
+
+// Object metodlari
+
+// let car = {
+// name: "BMW",
+// year: 2023,
+// color: "black"
+// }
+
+// kalitlarini massivga chiqarish
+// let keys = Object.keys(car)
+
+// qiymatlarini chiqarish
+// let values = Object.values(car)
+
+//
+// let entries = Object.entries(car)
+// console.log(entries);
+
+
+
+// function User(name, age) {
+//   this.name = name;
+//   this.age = age
+// }
+
+// let hayotbek = new User("Hayotbek", 15)
+// console.log(hayotbek);
+
+
+// class
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 }
-console.log(studentsFil(students));
+
+let John = new User('John', 23)
+
+console.log(John);
